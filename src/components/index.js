20a7fe53939a3bc62aabe3ae400c1014288eafed
joydeep1701 <joydeep1701@gmail.com>
2020-01-20 14:55:44 +0530

    Components Module

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import { NavBar } from './navbar';
+import { HomePage } from './home';
+import InterviewsPage from './interviews';
+import InterviewPage from './interview';
+import ParticipantsPage from './participants';
+
+export {
+  NavBar,
+  HomePage,
+  InterviewsPage,
+  InterviewPage,
+  ParticipantsPage,
+};
